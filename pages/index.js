a8d823c45cab88ffb97a3e0dd8ad59a6a0dc6495
Sonal Agrawal <sonalfeb7@gmail.com>
2024-06-01 16:08:28 +0530

    Add Meta Tag

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
       <Head>
         <title>Sonal-Protfolio</title>
         <link rel="icon" href="/logo.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <link
           rel="stylesheet"
           type="text/css"
