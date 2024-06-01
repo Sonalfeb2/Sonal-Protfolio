527ad4abf81686b5f9d4d127fd060cfe329ff424
Sonal Agrawal <sonalfeb7@gmail.com>
2024-06-01 15:58:41 +0530

    Changes Class to ClassName

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,10 @@ export default function Home() {
           Passionate about changing the world with technology
         </marquee>
         <div className={styles.contactIcons}>
-        <a class="link-dark" href="https://github.com/Sonalfeb2?tab=repositories" ><i className="fa fa-brands fa-github" /></a>
-        <a class="link-dark" href="https://www.linkedin.com/in/sonalagrawal02/"><i className="fa fa-brands fa-linkedin" /></a>
-        <a class="link-dark" href="https://www.facebook.com/sonal.agrawal.12139862/"><i className="fa fa-brands fa-facebook" /></a>
-        <a class="link-dark" href="https://www.instagram.com/sonal_feb2/"><i className="fa fa-brands fa-instagram" /></a>
+        <a className="link-dark" href="https://github.com/Sonalfeb2?tab=repositories" ><i className="fa fa-brands fa-github" /></a>
+        <a className="link-dark" href="https://www.linkedin.com/in/sonalagrawal02/"><i className="fa fa-brands fa-linkedin" /></a>
+        <a className="link-dark" href="https://www.facebook.com/sonal.agrawal.12139862/"><i className="fa fa-brands fa-facebook" /></a>
+        <a className="link-dark" href="https://www.instagram.com/sonal_feb2/"><i className="fa fa-brands fa-instagram" /></a>
         </div>
       </main>
       {/* Added All the Component of the navbar below */}
