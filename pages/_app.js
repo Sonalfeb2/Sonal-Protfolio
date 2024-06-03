e43c9c18bbe4415f4bc093a8774ed9f84d847dba
Sonal Agrawal <sonalfeb7@gmail.com>
2024-06-03 20:10:51 +0530

    Added Animation

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,49 @@
 import "../styles/globals.css";
 import Layout from "../components/layout/Layout";
-
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 function MyApp({ Component, pageProps }) {
+  
+  const [stars, setStars] = useState([]);
+  useEffect(() => {
+    const numStars = 50;
+    const newStars = [];
+
+    for (let i = 0; i < numStars; i++) {
+      const star = {
+        id: i,
+        left: `${Math.random() * 100}vw`,
+        animationDuration: `${Math.random() * 10 + 10}s`,
+        animationDelay: `${Math.random() * 10}s`,
+      };
+      newStars.push(star);
+    }
+
+    setStars(newStars);
+  }, []);
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+     <div className="starsContainer">
+        <div className="styles.stars">
+          {stars.map((star) => (
+            <div
+              key={star.id}
+              className="star"
+              style={{
+                left: star.left,
+                animationDuration: star.animationDuration,
+                animationDelay: star.animationDelay,
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+      <Layout style={{height:"200vh" ,padding:'20px'}}>
+        <Component {...pageProps} />
+      </Layout>
+    
+    </div>
   );
 }
 
